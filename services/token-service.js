4a14af0469a89f8815d62d24c171d044ef248218
AlexanderBuyanov <globalist72@gmail.com>
2022-02-10 00:49:16 +0200

    2022-02-10

--- services/token-service.js
+++ services/token-service.js
@@ -18,7 +18,6 @@ class TokenService {
     }
 
     validateAccessToken(token) {
-        console.log(token)
         try {
             const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET)
             return userData
